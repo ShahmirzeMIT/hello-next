b3ffe922854c31d42cba4b57b3a269117979a067
Shahmirze <85016768+ShahmirzeMIT@users.noreply.github.com>
2024-04-27 21:33:53 +0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  pageExtensions: ['mdx', 'md', 'jsx', 'js', 'tsx', 'ts'],
+  poweredByHeader: false,
+  productionBrowserSourceMaps: true,
   async redirects() {
     return [
       {

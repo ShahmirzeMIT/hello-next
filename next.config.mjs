e211f77f42af72a62da3008fa66af274b318e2c3
Shahmirze <85016768+ShahmirzeMIT@users.noreply.github.com>
2024-04-27 21:42:33 +0400

    uerrtiot

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  pageExtensions: ['mdx', 'md', 'jsx', 'js', 'tsx', 'ts'],
-  poweredByHeader: false,
-  productionBrowserSourceMaps: true,
-  async redirects() {
-    return [
-      {
-        source: '/about',
-        destination: '/',
-        permanent: true,
-      },
-    ];
-  },
+ 
 };
 
 export default nextConfig;
